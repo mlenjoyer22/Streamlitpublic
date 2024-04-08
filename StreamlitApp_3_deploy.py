431a70d7f4dd973eeccaf6d48375feb203683991
mlenjoyer22 <mlenjoyer22@gmail.com>
2024-04-08 20:44:11 +0300

    clearance

--- StreamlitApp_3_deploy.py
+++ StreamlitApp_3_deploy.py
@@ -1,22 +1,15 @@
 import streamlit as st
-import pandas as pd
 import requests
 import base64
-from transformers import ViTImageProcessor, ViTForImageClassification, EfficientNetImageProcessor, EfficientNetForImageClassification, AutoFeatureExtractor, ResNetForImageClassification
 from PIL import Image
-import torch
 
 API_URL_DICT = {
 "efficientnet-b7":"https://api-inference.huggingface.co/models/google/efficientnet-b7",
 "resnet-18":"https://api-inference.huggingface.co/models/microsoft/resnet-18",
 "vit-base-patch16-224":"https://api-inference.huggingface.co/models/google/vit-base-patch16-224"}
 
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer input_your_token"}
 
-
-
-
-# выбираем модель на боковой панели
 def input_features():
     model = st.selectbox("Модель", ("vit-base-patch16-224",
                                            "efficientnet-b7",
@@ -49,40 +42,7 @@ def query(model, data):
     response = requests.post(API_URL_DICT[model], headers=headers, data=data)
     return response.json()
 
-# def predict_local(model, data):   
-#     image = Image.open(data)
-#     if model == 'vit-base-patch16-224':
-#         processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
-#         model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
-#         inputs = processor(images=image, return_tensors="pt")
-#         outputs = model(**inputs)
-#         logits = outputs.logits
-#         # model predicts one of the 1000 ImageNet classes
-#         predicted_class_idx = logits.argmax(-1).item()
-#         return model.config.id2label[predicted_class_idx]
-#     if model == 'efficientnet-b7':
-#         preprocessor = EfficientNetImageProcessor.from_pretrained("google/efficientnet-b7")
-#         model = EfficientNetForImageClassification.from_pretrained("google/efficientnet-b7")
-#         inputs = preprocessor(image, return_tensors="pt")
-#         with torch.no_grad():
-#             logits = model(**inputs).logits
-#         # model predicts one of the 1000 ImageNet classes
-#         predicted_label = logits.argmax(-1).item()
-#         return model.config.id2label[predicted_label]
-#     if model == 'resnet-18':
-#         feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/resnet-18")
-#         model = ResNetForImageClassification.from_pretrained("microsoft/resnet-18")
-#         inputs = feature_extractor(image, return_tensors="pt")
-#         with torch.no_grad():
-#             logits = model(**inputs).logits
-#         # model predicts one of the 1000 ImageNet classes
-#         predicted_label = logits.argmax(-1).item()
-#         print(model.config.id2label[predicted_label])        
-
 def predict(model, flow, data):
-    # if flow == 'local':
-    #     result = predict_local(model, data)
-    # else:   
     result = query(model, data)
     return result
 
@@ -96,9 +56,7 @@ def inference(model, flow, upload):
         c1.image(im)        
         c2.header(f'Output {model}')
         c2.subheader('Predicted class :')
-        c2.write(output[0]['label'])
-        #st.balloons()
-
+        c2.write(output[0]['label'])        
     
 def show_main_page():    
     st.set_page_config(
@@ -117,7 +75,6 @@ def show_main_page():
 def process_main_page():
     show_main_page()
 
-    
 
 if __name__ == '__main__':
     process_main_page()
